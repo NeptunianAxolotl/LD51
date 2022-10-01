11ded72baea78b9fd8c00b5ebae261b4bb515f00
GoogleFrog <googl3frog@gmail.com>
2022-10-01 21:40:57 +1000

    Placeholder sawmill and town.

---
+++ defs/track/town.lua
@@ -0,0 +1,28 @@
+
+return {
+	stateImage = {"track_town"},
+	topImage = "town_over",
+	shopSlot = false,
+	paths = {
+		{
+			posFunc = function (t)
+				return {0.5 - t, 0}
+			end,
+			dirFunc = function (t)
+				return math.pi
+			end,
+			entry = 0,
+			destination = 2,
+		},
+		{
+			posFunc = function (t)
+				return {t - 0.5, 0}
+			end,
+			dirFunc = function (t)
+				return 0
+			end,
+			entry = 2,
+			destination = 0,
+		},
+	},
+}
