9dbdfbe6926382e304aa9c596cf9d24181b698e1
GoogleFrog <googl3frog@gmail.com>
2022-10-01 20:02:42 +1000

    Added selection ghost and rotating.

--- shopHandler.lua
+++ shopHandler.lua
@@ -1,5 +1,6 @@
 
 local util = require("include/util")
+local TrackDefs = util.LoadDefDirectory("defs/track")
 
 local api = {}
 local self = {}
@@ -27,15 +28,29 @@ function api.Update(dt)
 end
 
 function api.KeyPressed(key, scancode, isRepeat)
-
+	if key == "r" or key == "space" then
+		self.trackRotation = (self.trackRotation + 1)%4
+	end
 end
 
 function api.MousePressed(x, y)
 
 end
 
-function api.Draw(drwQueue)
-	
+function api.Draw(drawQueue)
+	if self.heldTrack then
+		local def = TrackDefs[self.heldTrack]
+		local pos = self.world.GetMousePosition()
+		drawQueue:push({y=1000; f=function()
+			local gx, gy = math.floor(pos[1] / Global.GRID_SIZE), math.floor(pos[2] / Global.GRID_SIZE)
+			if not (gx < 0 or gy < 0 or gx >= Global.WORLD_WIDTH or gy >= Global.WORLD_HEIGHT) then
+				love.graphics.setColor(1, 1, 1, 0.2)
+				love.graphics.setLineWidth(5)
+				love.graphics.rectangle("line", gx*Global.GRID_SIZE, gy*Global.GRID_SIZE, Global.GRID_SIZE, Global.GRID_SIZE, 4, 4, 8)
+			end
+			Resources.DrawImage(def.stateImage[1], pos[1], pos[2], self.trackRotation * math.pi/2, 0.8)
+		end})
+	end
 end
 
 function api.Initialize(world)
