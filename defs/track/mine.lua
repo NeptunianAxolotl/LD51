3534c3b4ae17e4e054ac02dfb93adab4b759da91
GoogleFrog <googl3frog@gmail.com>
2022-10-02 13:33:36 +1100

    Added farm and mine.

---
+++ defs/track/mine.lua
@@ -0,0 +1,32 @@
+
+return {
+	stateImage = {"track_mine"},
+	shopSlot = false,
+	trainMidFunc = function (self, train)
+		if not train.GetCarrying() then
+			train.SetCarrying("ore")
+		end
+	end,
+	paths = {
+		{
+			posFunc = function (t)
+				return {0.5 - t, 0}
+			end,
+			dirFunc = function (t)
+				return math.pi
+			end,
+			entry = 0,
+			destination = 2,
+		},
+		{
+			posFunc = function (t)
+				return {t - 0.5, 0}
+			end,
+			dirFunc = function (t)
+				return 0
+			end,
+			entry = 2,
+			destination = 0,
+		},
+	},
+}
